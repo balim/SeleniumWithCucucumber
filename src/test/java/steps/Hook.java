795f50916466ab3a9ee2c674b2aace78e38a6ba0
Mel Bali <bali_mel@hotmail.com>
2017-11-11 21:25:41 -0500

    new update

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -24,12 +24,12 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "C:\Users\Mel\Downloads\firefox-download\Libs\geckodriver.exe");
+        /*System.setProperty("webdriver.firefox.marionette", "C:\\Users\\Mel\\Downloads\\firefox-download\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "E:\Libs\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\Libs\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
